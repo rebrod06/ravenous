ba45fc72a64152ce1c25573520cc92bf4c50a0f6
Rebecca Rodriguez <rebrod06@gmail.com>
2023-09-12 21:04:40 -0600

    Ravenous Part 1 step 3

---
+++ src/Business.js
@@ -0,0 +1,22 @@
+// business component
+import React from 'react';
+
+function Business(prop) {
+  return (
+    <div>
+      <img classname="image"/>
+      <p classname="name"></p>
+      <p classname="address"></p>
+      <p classname="city"></p>
+      <p classname="state"></p>
+      <p classname="zipcode"></p>
+      <p classname="category"></p>
+      <p classname="rating"></p>
+      <p classname="reviews"></p>
+  </div>
+  )
+}
+
+
+
+export default Business;
